a697f4cac9da50e537deacaa3ea107b211f0c25b
Johnson <johnsonphan95@gmail.com>
2020-03-01 22:37:37 -0800

    Add latest block from web3

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,45 @@ import Web3 from 'web3';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      blockNumber: 0, 
+      difficulty: 0, 
+      gasPrice: 0, 
+      latestBlocks: []
+    }
+  }
+
   async componentWillMount() {
     let web3 = new Web3(infura_url);
     let latestBlock = await web3.eth.getBlock('latest');
+
     console.log(latestBlock)
+
+    this.setState({
+      blockNumber: latestBlock.number, 
+      difficulty: latestBlock.difficulty
+    })
+
+    let gasPrice = await web3.eth.getGasPrice();
+    this.setState({
+      gasPrice: gasPrice
+    })
   }
 
   render() {
     return (
-      <div>
-        Test :)
+      <div className="main-container">
+        <div>
+          Latest Block: {this.state.blockNumber}
+        </div>
+        <div>
+          Difficulty: {this.state.difficulty}
+        </div>
+        <div>
+          Gas Price: {this.state.gasPrice}
+        </div>
       </div>
     )
   }
