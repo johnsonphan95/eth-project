052b4afacd367c4e61df1eefa473428d62f931ec
Johnson <johnsonphan95@gmail.com>
2020-03-01 22:21:32 -0800

    Add infura_url

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import {infura_url} from '../_private/keys';
 import './App.css';
 import Web3 from 'web3';
 
 class App extends Component {
+
+  async componentWillMount() {
+    let web3 = new Web3(infura_url);
+    let latestBlock = await web3.eth.getBlock('latest');
+    console.log(latestBlock)
+  }
+
   render() {
     return (
       <div>
